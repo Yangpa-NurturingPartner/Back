5d20f9078d7c2dfc63b44006307c2535036687be
jeongmin choi <jeongmin0521@naver.com>
2024-09-23 16:37:36 +0900

    글 목록 조회

--- src/main/java/com/example/demo/service/CommunityMapper.java
+++ src/main/java/com/example/demo/service/CommunityMapper.java
@@ -1,4 +1,17 @@
 package com.example.demo.service;
 
-public class CommunityMapper {
+import com.example.demo.model.CommunityBoardVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface CommunityMapper {
+
+    @Select("SELECT * FROM community_board ORDER BY board_no DESC LIMIT #{size} OFFSET #{skip}")
+    List<CommunityBoardVO> getBoardList(int size, int skip);
+
+    @Select("SELECT COUNT(*) FROM community_board")
+    int getTotalCount();
 }
